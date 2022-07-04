faf5357d5a40818358e257e4e07f311d01a4786f
danycambrano <danycambrano1@gmail.com>
2022-07-04 14:10:53 -0500

    Inicio de nuestro servidor

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
+import mongoose from 'mongoose';
+
+
+mongoose.Promise=global.Promise;
+const dbURL='mongodb://localhost:27017/movil';
+mongoose.connect(dbURL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(mongoose=>console.log('Conectado a la BD en el puerto 27017'))
+.catch(err=>console.log(err));
+
+
+const app=express();
+app.set('port', process.env.PORT || 4000);
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'),()=>{
+    console.log('Servidor ejecutado en el puerto:'+app.get('port'));
+});
